be2a8f79f3ffdbf8fe45fa9f6f2a701fc4739991
Yuichi Takada <takadyuichi@gmail.com>
2015-04-19 00:00:44 +0900

    add title column

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -69,6 +69,6 @@ class MemosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def memo_params
-      params.require(:memo).permit(:md)
+      params.require(:memo).permit(:title, :md)
     end
 end
--- db/migrate/20150416070232_create_memos.rb
+++ db/migrate/20150416070232_create_memos.rb
@@ -1,6 +1,7 @@
 class CreateMemos < ActiveRecord::Migration
   def change
     create_table :memos do |t|
+      t.string :title
       t.string :md
 
       t.timestamps null: false
