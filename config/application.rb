2e08fb93b64627b86ee46c61495fe9da68cc7e4b
Yuichi Takada <takadyuichi@gmail.com>
2015-04-30 20:13:26 +0900

    disable routing_specs and view_specs

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,13 @@ module WebMemo
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.generators.template_engine = :slim
+    config.generators do |g|
+      g.template_engine :slim
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl
+      g.view_specs false
+      g.routing_specs false
+    end
+
   end
 end
